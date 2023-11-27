446764441a01ee86309d378323c0c8545d3d2cba
Tim Mitra <tim@it-guy.com>
2023-11-27 00:58:43 -0500

    Initial Commit

---
+++ MyBooks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyBooks
+//
+//  Created by Tim Mitra on 2023-11-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyBooks/MyBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBooksApp.swift
+//  MyBooks
+//
+//  Created by Tim Mitra on 2023-11-27.
+//
+
+import SwiftUI
+
+@main
+struct MyBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
