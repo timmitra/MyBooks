1aa936602657d07f48ffb37399c4b0d8b23fbf4a
Tim Mitra <tim@it-guy.com>
2023-11-27 01:23:16 -0500

    set up modelContainer and print path to the data store

---
+++ MyBooks/NewBookView.swift
@@ -0,0 +1,18 @@
+//
+//  NewBookView.swift
+//  MyBooks
+//
+//  Created by Tim Mitra on 2023-11-27.
+//
+
+import SwiftUI
+
+struct NewBookView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewBookView()
+}
