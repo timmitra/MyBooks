472a9d69db120a0f5570b5f831d3c6aeaf0d260d
Tim Mitra <tim@it-guy.com>
2023-11-27 03:36:23 -0500

    added Edit View and Rating

---
+++ MyBooks/EditBookView.swift
@@ -0,0 +1,18 @@
+//
+//  EditBookView.swift
+//  MyBooks
+//
+//  Created by Tim Mitra on 2023-11-27.
+//
+
+import SwiftUI
+
+struct EditBookView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditBookView()
+}
