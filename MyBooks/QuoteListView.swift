d6b7c8f2792b671403160bbd180e91d3f84787ce
Tim Mitra <tim@it-guy.com>
2023-11-28 12:11:04 -0500

    should be "quotes"

---
+++ MyBooks/QuoteListView.swift
@@ -0,0 +1,18 @@
+//
+//  QuoteListView.swift
+//  MyBooks
+//
+//  Created by Tim Mitra on 2023-11-28.
+//
+
+import SwiftUI
+
+struct QuoteListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuoteListView()
+}
