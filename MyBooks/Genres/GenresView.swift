b42ce2df461572aad0634ac7eb89ea7f9a4a2628
Tim Mitra <tim@it-guy.com>
2023-12-03 22:57:01 -0500

    organizing, added NewGenreView and GenreView

---
+++ MyBooks/Genres/GenresView.swift
@@ -0,0 +1,18 @@
+//
+//  GenresView.swift
+//  MyBooks
+//
+//  Created by Tim Mitra on 2023-12-03.
+//
+
+import SwiftUI
+
+struct GenresView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GenresView()
+}
---
+++ MyBooks/Genres/NewGenreView.swift
@@ -0,0 +1,18 @@
+//
+//  NewGenreView.swift
+//  MyBooks
+//
+//  Created by Tim Mitra on 2023-12-03.
+//
+
+import SwiftUI
+
+struct NewGenreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewGenreView()
+}
