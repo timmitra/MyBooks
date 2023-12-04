e579e1cde1637ed6c1db35a5b14bc9e4c4aa75f8
Tim Mitra <tim@it-guy.com>
2023-12-03 22:59:33 -0500

    coding NewGenreView with colorpicker

--- MyBooks/Genres/NewGenreView.swift
+++ MyBooks/Genres/NewGenreView.swift
@@ -6,10 +6,32 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct NewGenreView: View {
+  @Environment(\.modelContext) var context
+  @Environment(\.dismiss) var dismiss
+  @State private var name = ""
+  @State private var color = Color.red
+  
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      NavigationStack {
+        Form {
+          TextField("Name", text: $name)
+          ColorPicker("Set the genre color", selection: $color, supportsOpacity: false)
+          Button("Create") {
+            let newGenre = Genre(name: name, color: color.toHexString()!)
+            context.insert(newGenre)
+            dismiss()
+          }
+          .buttonStyle(.borderedProminent)
+          .frame(maxWidth: .infinity, alignment: .trailing)
+          .disabled(name.isEmpty)
+        }
+        .padding()
+        .navigationTitle("New Genre")
+        .navigationBarTitleDisplayMode(.inline)
+      }
     }
 }
 
