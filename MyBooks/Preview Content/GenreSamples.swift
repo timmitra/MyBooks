49bfda57f1e9a4189cba3648157726248dfaa266
Tim Mitra <tim@it-guy.com>
2023-12-03 22:57:45 -0500

    removing trailing comma

--- MyBooks/Preview Content/GenreSamples.swift
+++ MyBooks/Preview Content/GenreSamples.swift
@@ -13,7 +13,7 @@ extension Genre {
     Genre(name: "Fiction", color: "00FF00"),
     Genre(name: "Non-fiction", color: "0000FF"),
     Genre(name: "Romantic", color: "FF0000"),
-    Genre(name: "Thriller", color: "000000"),
+    Genre(name: "Thriller", color: "000000")
     ]
   }
 }
