b6d54efd448d3ad4ca55b87ddb2618cb6afb0be6
Tim Mitra <tim@it-guy.com>
2023-12-03 22:58:37 -0500

    coding the genre view and preview

--- MyBooks/Genres/GenresView.swift
+++ MyBooks/Genres/GenresView.swift
@@ -6,13 +6,111 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct GenresView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  @Environment(\.dismiss) private var dismiss
+  @Environment(\.modelContext) private var context
+  @Bindable var book: Book // be able to update properties
+  @Query(sort: \Genre.name) var genres: [Genre]
+  @State private var newGenre = false
+  
+  var body: some View {
+    NavigationStack {
+      Group {
+        if genres.isEmpty {
+          ContentUnavailableView {
+            Image(systemName: "bookmark.fill")
+          } description: {
+            Text("You need to create some genres.")
+          } actions: {
+            Button("Create Genre") {
+              newGenre.toggle()
+            }
+            .buttonStyle(.borderedProminent)
+          }
+        } else {
+          List {
+            ForEach(genres) { genre in
+              HStack {
+                if let bookGenres = book.genres {
+                  if bookGenres.isEmpty {
+                    Button {
+                      addRemove(genre)
+                    } label: {
+                      Image(systemName: "circle")
+                    }
+                    .foregroundColor(genre.hexColor)
+                  } else {
+                    Button {
+                      addRemove(genre)
+                    } label: {
+                      Image(systemName: bookGenres.contains(genre) ? "circle.fill" : "circle" )
+                    }
+                    .foregroundColor(genre.hexColor)
+                  }
+                }
+                Text(genre.name)
+              }
+            }
+            LabeledContent {
+              Button {
+                newGenre.toggle()
+              } label: {
+                Image(systemName: "plus.circle.fill")
+                  .imageScale(.large)
+              }
+              .buttonStyle(.borderedProminent)
+            } label: {
+              Text("Create new genre")
+                .font(.caption)
+                .foregroundStyle(.secondary)
+            }
+          }
+        }
+      }
+      .navigationTitle(book.title)
+      .sheet(isPresented: $newGenre) {
+        NewGenreView()
+      }
+      .toolbar{
+        ToolbarItem(placement: .topBarLeading) {
+          Button("Back") {
+            dismiss()
+          }
+        }
+      }
     }
-}
+  }
+  func addRemove(_ genre: Genre) {
+    if let bookGenres = book.genres {
+      if bookGenres.isEmpty {
+        book.genres?.append(genre)
+      } else {
+        if bookGenres.contains(genre),
+           let index = bookGenres.firstIndex(where: {
+             $0.id == genre.id
+           }) {
+          book.genres?.remove(at: index)
+        } else {
+          book.genres?.append(genre)
+        }
+        }
+      }
+    }
+  }
+
 
 #Preview {
-    GenresView()
+  let preview = Preview(Book.self)
+  let books = Book.sampleBooks
+  let genres = Genre.sampleGenres
+   // add examples into preview provider
+  preview.addExamples(books)
+  preview.addExamples(genres)
+  // append a genre
+  books[1].genres?.append(genres[0])
+  return GenresView(book: books[1])
+    .modelContainer(preview.container)
+  // using preview container for modelContainer
 }
